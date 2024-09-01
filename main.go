509e8362e10b69eac095c2753726d14fd0450a86
Yugo Terada <yugo@ensemble-lab.com>
2024-09-02 03:32:11 +0900

    db への接続を実装

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/aspick/wtest/src/config"
 	"github.com/aspick/wtest/src/handler"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // doMain はメイン関数の本体です。
 func doMain() int {
 	// TODO: Graceful shut down を追加する
 
+	env := config.Env{}
+
+	pool, err := setupDB(env)
+	if err != nil {
+		fmt.Printf("failed to setup db: %v", err)
+		return 1
+	}
+
 	mux := http.NewServeMux()
-	handler.RegisterHandlers(mux)
+	handler.RegisterHandlers(mux, pool)
 
 	http.ListenAndServe(":8080", mux)
 
 	return 0
+}
+
+func setupDB(env config.Env) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), env.GetDBURL())
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
 }
 
 func main() {
